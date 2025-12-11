cebfeedfa705966d79db9cee759a7a24086e3786
Tommy Zhou <tommyzhou03@gmail.com>
2025-12-11 08:51:56 +0800

    fix env variables

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 import axios from "axios";
 
-const { API_URL } = import.meta.env.API_URL;
+const API_URL = import.meta.env.API_URL;
 
 function Navbar() {
     const [user, setUser] = useState(null);
