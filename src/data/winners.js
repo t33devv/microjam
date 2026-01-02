36612d13a1ebab2ed9df5a044540ccd1eb788e8d
BDvok2 <dvorakbodion020@gmail.com>
2026-01-02 14:38:15 +0200

    added hall of fame

---
+++ src/data/winners.js
@@ -0,0 +1,370 @@
+export const winners = [
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-014",
+        gameName: "Tiny Situation",
+        gameUrl: "https://seeone.itch.io/tinysituation",
+        contributors: [
+            {
+                discordId: "611423346252578828",
+                discordUrl: "https://discord.com/users/611423346252578828",
+            },
+        ]
+        
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-014",
+        gameName: "Tiny City",
+        gameUrl: "https://memiczny.itch.io/tiny-city",
+        contributors: [
+            {
+                discordId: "608040417010909185",
+                discordUrl: "https://discord.com/users/608040417010909185",
+            },
+        ]
+        
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-014",
+        gameName: "Helping Hand",
+        gameUrl: "https://alexio-games.itch.io/helping-hand",
+        contributors: [
+            {
+                discordId: "403854404836655114",
+                discordUrl: "https://discord.com/users/403854404836655114",
+            },
+        ]
+        
+    },
+
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-014",
+        gameName: "Zombie Box",
+        gameUrl: "https://smc24dev.itch.io/zombie-box",
+        contributors: [
+            {
+                discordId: "",
+                discordUrl: "",
+            },
+        ]
+        
+    },
+   
+
+
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-016",
+        gameName: "Space Junkers",
+        gameUrl: "https://ogelgames.itch.io/space-junkers",
+        contributors: [
+            {
+                discordId: "626420812198576138",
+                discordUrl: "https://discord.com/users/626420812198576138",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-016",
+        gameName: "Burning Void",
+        gameUrl: "https://gormlessgames.itch.io/burning-void",
+        contributors: [
+            {
+                discordId: "464877728831897601",
+                discordUrl: "https://discord.com/users/464877728831897601",
+            },
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-016",
+        gameName: "Planet Jump",
+        gameUrl: "https://florian-castel.itch.io/planet-jump",
+        contributors: [
+            {
+                discordId: "363384068915855360",
+                discordUrl: "https://discord.com/users/363384068915855360",
+            },
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-016",
+        gameName: "Mutant Ship",
+        gameUrl: "https://donnadie.itch.io/mutant-ship",
+        contributors: [
+            {
+                discordId: "331889644042190868",
+                discordUrl: "https://discord.com/users/331889644042190868",
+            },
+        ]
+        
+    },
+
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-017",
+        gameName: "Flooded Isles",
+        gameUrl: "https://subsolar.itch.io/flooded-isles",
+        contributors: [
+            {
+                discordId: "973746422002380820",
+                discordUrl: "https://discord.com/users/973746422002380820",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-017",
+        gameName: "Ghosts and Gold",
+        gameUrl: "https://heuzy.itch.io/ghosts-and-gold",
+        contributors: [
+            {
+                discordId: "1090444437668253827",
+                discordUrl: "https://discord.com/users/1090444437668253827",
+            },
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-017",
+        gameName: "Stranded Shores",
+        gameUrl: "https://zapzoop0099.itch.io/stranded-shores",
+        contributors: [
+            {
+                discordId: "1002295729403994113 ",
+                discordUrl: "https://discord.com/users/1002295729403994113",
+            },
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-017",
+        gameName: "Mutant Ship",
+        gameUrl: "https://smc24dev.itch.io/maou-demo",
+        contributors: [
+            {
+                discordId: "1253265904645570605",
+                discordUrl: "https://discord.com/users/1253265904645570605",
+            },
+            {
+                discordId: "165864255319572480",
+                discordUrl: "https://discord.com/users/165864255319572480",
+            },
+        ]
+        
+    },
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-018",
+        gameName: "Sub Optimal",
+        gameUrl: "https://florian-castel.itch.io/sub-optimal",
+        contributors: [
+            {
+                discordId: "1002295729403994113 ",
+                discordUrl: "https://discord.com/users/1002295729403994113",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-018",
+        gameName: "Gutshot",
+        gameUrl: "https://ne-mene.itch.io/gutshot",
+        contributors: [
+            {
+                discordId: "1112843835643543612 ",
+                discordUrl: "https://discord.com/users/1112843835643543612",
+            },
+            {
+                discordId: "634387545500680203 ",
+                discordUrl: "https://discord.com/users/634387545500680203",
+            },
+            {
+                discordId: "733618498667937842 ",
+                discordUrl: "https://discord.com/users/733618498667937842",
+            },
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-018",
+        gameName: "Firextinguisher",
+        gameUrl: "https://dogedev67.itch.io/firextinguisher",
+        contributors: [
+            {
+                discordId: "757134253375881269 ",
+                discordUrl: "https://discord.com/users/757134253375881269",
+            },
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-018",
+        gameName: "Sub Optimal",
+        gameUrl: "https://florian-castel.itch.io/sub-optimal",
+        contributors: [
+            {
+                discordId: "363384068915855360",
+                discordUrl: "https://discord.com/users/363384068915855360",
+            },
+            
+        ]
+        
+    },
+
+
+    
+    
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-019",
+        gameName: "The Eternal Night",
+        gameUrl: "https://mr-spirit.itch.io/the-eternal-night",
+        contributors: [
+            {
+                discordId: "1218823155595083806 ",
+                discordUrl: "https://discord.com/users/1218823155595083806",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-019",
+        gameName: "Spatial Shift",
+        gameUrl: "https://oongus-boongus.itch.io/spatial-shift",
+        contributors: [
+            {
+                discordId: "1001936966687932467 ",
+                discordUrl: "https://discord.com/users/1001936966687932467",
+            },
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-019",
+        gameName: "Grid Jump",
+        gameUrl: "https://donmusborek.itch.io/grid-jump",
+        contributors: [
+            {
+                discordId: "486948400068034570 ",
+                discordUrl: "https://discord.com/users/486948400068034570",
+            },
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-019",
+        gameName: "Dimensional Drifter",
+        gameUrl: "https://smc24dev.itch.io/dimensional-drifter",
+        contributors: [
+            {
+                discordId: "1253265904645570605",
+                discordUrl: "https://discord.com/users/1253265904645570605",
+            },
+            
+        ]
+        
+    },
+    
+    
+    
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-020",
+        gameName: "Sand of Time",
+        gameUrl: "https://cupofgames.itch.io/sand-of-time",
+        contributors: [
+            {
+                discordId: "1133098912962584728",
+                discordUrl: "https://discord.com/users/1133098912962584728",
+            },
+            {
+                discordId: "1056710586911703143",
+                discordUrl: "https://discord.com/users/1056710586911703143",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-020",
+        gameName: "EchoShift",
+        gameUrl: "https://bittybigstudio.itch.io/echoshift-microjam-20-time",
+        contributors: [
+            {
+                discordId: "530779280779771913",
+                discordUrl: "https://discord.com/users/530779280779771913",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-020",
+        gameName: "Ultra Cold",
+        gameUrl: "https://akwedukt.itch.io/ultra-cold",
+        contributors: [
+            {
+                discordId: "996843745397985291",
+                discordUrl: "https://discord.com/users/996843745397985291",
+            },
+            
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-020",
+        gameName: "Demake",
+        gameUrl: "https://smc24dev.itch.io/demake",
+        contributors: [
+            {
+                discordId: "1253265904645570605",
+                discordUrl: "https://discord.com/users/1253265904645570605",
+            },
+            
+        ]
+        
+    },
+    
+
+]
