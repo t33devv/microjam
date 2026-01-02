daeb48c9cb6e7be7793aedaea963b0b4bc140496
BDvok2 <dvorakbodion020@gmail.com>
2026-01-02 17:08:08 +0200

    did up to jam-28

--- src/data/winners.js
+++ src/data/winners.js
@@ -470,5 +470,284 @@ export const winners = [
     },
 
 
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-023",
+        gameName: "Heat",
+        gameUrl: "https://janslav.itch.io/heat",
+        contributors: [
+            {
+                discordId: "380789537938604037",
+                discordUrl: "https://discord.com/users/380789537938604037",
+            },
+            {
+                discordId: "729800937316548671",
+                discordUrl: "https://discord.com/users/729800937316548671",
+            },
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-023",
+        gameName: "Lethal Potato",
+        gameUrl: "https://crypt1x.itch.io/lethal-potato",
+        contributors: [
+            {
+                discordId: "766986295771267102",
+                discordUrl: "https://discord.com/users/766986295771267102",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-023",
+        gameName: "Plane Racer: Overheat",
+        gameUrl: "https://swordsong.itch.io/plane-racer-overheat",
+        contributors: [
+            {
+                discordId: "",
+                discordUrl: "",
+            },
+            
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-023",
+        gameName: "Dig Deeper",
+        gameUrl: "https://smc24dev.itch.io/dig-deeper",
+        contributors: [
+            {
+                discordId: "1253265904645570605",
+                discordUrl: "https://discord.com/users/1253265904645570605",
+            },
+            
+        ],
+    },
+
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-024",
+        gameName: "Just Us",
+        gameUrl: "https://markositta.itch.io/just-us",
+        contributors: [
+            {
+                discordId: "634387545500680203",
+                discordUrl: "https://discord.com/users/634387545500680203",
+            },
+            
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-024",
+        gameName: "Skyfall",
+        gameUrl: "https://seeone.itch.io/skyfall",
+        contributors: [
+            {
+                discordId: "611423346252578828",
+                discordUrl: "https://discord.com/users/611423346252578828",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-024",
+        gameName: "Wind Trawlers",
+        gameUrl: "https://tofu-boy.itch.io/wind-trawlers",
+        contributors: [
+            {
+                discordId: "1292101999042760735",
+                discordUrl: "https://discord.com/users/1292101999042760735",
+            },
+            
+        ],
+    },
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-024",
+        gameName: "Whispers of the Wind",
+        gameUrl: "https://iamst.itch.io/whispers-of-the-wind",
+        contributors: [
+            {
+                discordId: "",
+                discordUrl: "",
+            },
+            
+        ],
+    },
+
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-025",
+        gameName: "Bloodbound Escape",
+        gameUrl: "https://laggyfox.itch.io/bloodbound-escape",
+        contributors: [
+            {
+                discordId: "1298188767089262624",
+                discordUrl: "https://discord.com/users/1298188767089262624",
+            },
+            
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-025",
+        gameName: "Vampire Bat",
+        gameUrl: "https://itsbluerock.itch.io/vampire-bat",
+        contributors: [
+            {
+                discordId: "1283473714544906325",
+                discordUrl: "https://discord.com/users/1283473714544906325",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-025",
+        gameName: "Vampire Rampage",
+        gameUrl: "https://djhaggz.itch.io/vampire-rampage",
+        contributors: [
+            {
+                discordId: "845517719674552383",
+                discordUrl: "https://discord.com/users/845517719674552383",
+            },
+            
+        ],
+    },
+    {
+        place: "1",
+        category: "miniscript",
+        jam: "micro-jam-025",
+        gameName: "Vampire Catch",
+        gameUrl: "https://watercooling.itch.io/vampire-catch",
+        contributors: [
+            {
+                discordId: "456226577798135808",
+                discordUrl: "https://discord.com/users/456226577798135808",
+            },
+            
+        ],
+    },
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-026",
+        gameName: "Catnip",
+        gameUrl: "https://vtsz.itch.io/catnip",
+        contributors: [
+            {
+                discordId: "1043674527852417164",
+                discordUrl: "https://discord.com/users/1043674527852417164",
+            },
+            
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-026",
+        gameName: "Volley Cats",
+        gameUrl: "https://clownfishcreations.itch.io/volley-cats",
+        contributors: [
+            {
+                discordId: "774146825812312084",
+                discordUrl: "https://discord.com/users/774146825812312084",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-026",
+        gameName: "Rooftop Drop",
+        gameUrl: "https://djhaggz.itch.io/rooftop-drop",
+        contributors: [
+            {
+                discordId: "845517719674552383",
+                discordUrl: "https://discord.com/users/845517719674552383",
+            },
+            
+        ],
+    },
+
+
+
+
+    {
+        place: "1",
+        category: "overall",
+        jam: "micro-jam-027",
+        gameName: "Broken Game Console",
+        gameUrl: "https://voidcattgames.itch.io/broken-game-console",
+        contributors: [
+            {
+                discordId: "1307683319399321644",
+                discordUrl: "https://discord.com/users/1307683319399321644",
+            },
+            
+        ],
+    },
+    {
+        place: "2",
+        category: "overall",
+        jam: "micro-jam-027",
+        gameName: "Skye's Adventure 64",
+        gameUrl: "https://gm637legend.itch.io/skyes-adventure-64",
+        contributors: [
+            {
+                discordId: "532728970135732224",
+                discordUrl: "https://discord.com/users/532728970135732224",
+            },
+            
+        ],
+    },
+    {
+        place: "3",
+        category: "overall",
+        jam: "micro-jam-027",
+        gameName: "Torchborn",
+        gameUrl: "https://vinkesoft.itch.io/torchborn",
+        contributors: [
+            {
+                discordId: "347753576929689600",
+                discordUrl: "https://discord.com/users/347753576929689600",
+            },
+            {
+                discordId: "476011861385412627",
+                discordUrl: "https://discord.com/users/476011861385412627",
+            },
+            
+        ],
+    },
+    
 ]
 
